5da5463827d4090bd747a9c199b9b0214451d833
KimSo-jung <102447864+KimSo-jung@users.noreply.github.com>
2024-03-26 16:28:15 +0900

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -10,116 +10,248 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'BoogieWeather',
+      home: SomeWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class SomeWidget extends StatelessWidget {
+  const SomeWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        width: 393,
+        height: 852,
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+            colors: [Color(0xFF73D5FF), Color(0xFFBED5DE)],
+          ),
+        ),
+        child: Stack(
+          children: [
+            const Positioned(
+              left: 159,
+              top: 65,
+              child: SizedBox(
+                width: 202,
+                height: 30,
+                child: Text(
+                  '즐겨찾는 위치',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Positioned(
+              left: 32,
+              top: 203,
+              child: Container(
+                width: 329,
+                height: 129,
+                decoration: ShapeDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 53,
+              top: 263,
+              child: SizedBox(
+                width: 47,
+                height: 47,
+                child: Stack(children: []),
+              ),
+            ),
+            const Positioned(
+              left: 48,
+              top: 216,
+              child: SizedBox(
+                width: 129,
+                child: Text(
+                  '잠실야구장',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 32,
+              top: 353,
+              child: Container(
+                width: 329,
+                height: 129,
+                decoration: ShapeDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 32,
+              top: 503,
+              child: Container(
+                width: 329,
+                height: 129,
+                decoration: ShapeDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 49,
+              top: 559.51,
+              child: SizedBox(
+                width: 59,
+                height: 51.93,
+                child: Stack(children: []),
+              ),
+            ),
+            const Positioned(
+              left: 48,
+              top: 366,
+              child: SizedBox(
+                width: 179,
+                child: Text(
+                  '대구삼성\n라이온즈파크',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 48,
+              top: 516,
+              child: SizedBox(
+                width: 179,
+                child: Text(
+                  '창원NC파크',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 316,
+              top: 216,
+              child: SizedBox(
+                width: 29,
+                height: 29,
+                child: Stack(children: []),
+              ),
+            ),
+            const Positioned(
+              left: 234,
+              top: 225,
+              child: Text(
+                '9°',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 64,
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 205,
+              top: 380,
+              child: Text(
+                '12°',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 64,
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 205,
+              top: 528,
+              child: Text(
+                '14°',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 64,
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 32,
+              top: 114,
+              child: Container(
+                width: 329,
+                height: 59,
+                decoration: ShapeDecoration(
+                  color: Colors.white.withOpacity(0.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(34),
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 51,
+              top: 128,
+              child: SizedBox(
+                width: 129,
+                child: Text(
+                  '찾는 위치...',
+                  style: TextStyle(
+                    color: Color(0xFF787878),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              left: 21,
+              top: 58,
+              child: SizedBox(
+                width: 32,
+                height: 32,
+                child: Stack(children: []),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
