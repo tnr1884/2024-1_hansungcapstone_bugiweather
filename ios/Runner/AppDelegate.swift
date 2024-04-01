b64b7ed90e1e7757158b92d29786e49be2b4fd80
tnr1884 <tnr1884@hansung.ac.kr>
2024-04-01 21:44:06 +0900

    핀추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,3 +11,12 @@ import Flutter
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+if (CLLocationManager.locationServicesEnabled()) {
+    switch CLLocationManager.authorizationStatus() {
+    case .denied, .notDetermined, .restricted:
+        self.manager.requestAlwaysAuthorization()
+        break
+    default:
+        break
+    }
+}
