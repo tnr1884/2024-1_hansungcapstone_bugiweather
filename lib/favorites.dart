00a377b13deb088b3a932c0b6695c7ea986b2ec1
PARK\tnr18 <tnr1884@hansung.ac.kr>
2024-04-30 14:33:29 +0900

    favorites 위젯 크기 조정

--- lib/favorites.dart
+++ lib/favorites.dart
@@ -11,7 +11,7 @@ class Favorites extends StatelessWidget {
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30.0)
           ),
-          backgroundColor: Colors.lightBlue,
+          //backgroundColor: Colors.lightBlue,
           onPressed: (){
             //onPressed Actions
           },
@@ -19,6 +19,8 @@ class Favorites extends StatelessWidget {
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
         body: Container(
+          width: double.infinity,
+          height: double.infinity,
           clipBehavior: Clip.antiAlias,
           decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -27,107 +29,109 @@ class Favorites extends StatelessWidget {
                   colors: [Color(0xff73d5ff), Color(0xffbed5de)]
               )
           ),
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30.0),
-                    color: Colors.white.withOpacity(0.5)
+          //padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+          child: Center(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      color: Colors.white.withOpacity(0.5)
+                  ),
+                  alignment: Alignment.centerLeft,
+                  height: 60, width: 350,
+                  padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  child: Text("위치 검색...",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black.withOpacity(0.3)
+                    ),),
                 ),
-                alignment: Alignment.centerLeft,
-                height: 60, width: 350,
-                padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                child: Text("위치 검색...",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black.withOpacity(0.3)
-                  ),),
-              ),
-              const Padding(padding: EdgeInsets.fromLTRB(0, 80, 200, 500),
-                child: Text(
-                  "즐겨찾는 위치",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
+                const Padding(padding: EdgeInsets.fromLTRB(0, 80, 200, 500),
+                  child: Text(
+                    "즐겨찾는 위치",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 40),
-              SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 0),
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  children: [
-                    Container(
-                      height: 130, width: 330,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: Colors.white.withOpacity(0.5)
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: const Column(
-                        children: [
-                          SizedBox(
-                            child: Text(
-                              "Forecast data here",
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black87,
+                const SizedBox(height: 40),
+                SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(vertical: 120, horizontal: 0),
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 130, width: 330,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.white.withOpacity(0.5)
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        child: const Column(
+                          children: [
+                            SizedBox(
+                              child: Text(
+                                "Forecast data here",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.black87,
+                                ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      height: 130, width: 330,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: Colors.white.withOpacity(0.5)
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: const Column(
-                        children: [
-                          SizedBox(
-                            child: Text(
-                              "Forecast data here",
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black87,
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 130, width: 330,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.white.withOpacity(0.5)
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        child: const Column(
+                          children: [
+                            SizedBox(
+                              child: Text(
+                                "Forecast data here",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.black87,
+                                ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      height: 130, width: 330,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          color: Colors.white.withOpacity(0.5)
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                      child: const Column(
-                        children: [
-                          SizedBox(
-                            child: Text(
-                              "Forecast data here",
-                              style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.black87,
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 130, width: 330,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.white.withOpacity(0.5)
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        child: const Column(
+                          children: [
+                            SizedBox(
+                              child: Text(
+                                "Forecast data here",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.black87,
+                                ),
                               ),
-                            ),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         )
     );
